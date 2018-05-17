8e3e576245691895fc11588ae8e9825e90e55b8c
Daniel Burke <daniel.burke@magellangroup.net>
2018-05-17 13:47:33 +0100

    Finished 6.9

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def __str__(self):  
         return self.name
@@ -14,3 +16,4 @@ class Page(models.Model):
 
     def __str__(self):     
         return self.title
+
